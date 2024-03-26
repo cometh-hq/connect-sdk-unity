46083bd49d05a94a56358fb3567a128cc2b54721
Ludovic_Domingues <ludovic.domingues96@gmail.com>
2024-03-26 10:46:09 +0100

    Removed a lot of unused scripts

--- Assets/ComethSDK/Scripts/HTTP/Responses/ConnectToComethWalletResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ComethSDK.Scripts.HTTP.Responses
-{
-	public class ConnectToComethWalletResponse
-	{
-		public bool success { get; set; }
-		public string walletAddress { get; set; }
-		public string isDeployed { get; set; }
-	}
-}
