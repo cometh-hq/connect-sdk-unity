b28dc838fcc3b743c0c7d3bc9132b04291c0392c
Benjamin Payoux <1423027+benjaminpayoux@users.noreply.github.com>
2024-06-25 17:15:13 +0200

    Update Exceptions.cs

--- Assets/ComethSDK/Scripts/Types/Exceptions.cs
+++ Assets/ComethSDK/Scripts/Types/Exceptions.cs
@@ -1,6 +1,16 @@
 using System;
 
-public class SignerNotFoundException : Exception
+public class SignerException : Exception
+{
+    public SignerException(string message) : base(message) { }
+}
+
+public class SignerNotFoundException : SignerException
 {
     public SignerNotFoundException(string message) : base(message) { }
+}
+
+public class SignerUnauthorizedException : SignerException
+{
+    public SignerUnauthorizedException(string message) : base(message) { }
 }
